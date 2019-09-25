33a69757f2c7d9e4076a3cbf45aaa096cedcd286
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-09-26 00:31:09 +0200

    Reformatted LICENSE. Added .nuspec.

--- src/Nullable/NullableAttributes.cs
+++ src/Nullable/NullableAttributes.cs
@@ -1,4 +1,31 @@
-﻿#if !NULLABLE_ATTRIBUTES_DISABLE
+﻿#region License
+// MIT License
+// 
+// Copyright (c) 2019 Manuel Römer
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+
+// This code file is automatically added by the Nullable NuGet package.
+// Please see https://github.com/manuelroemer/Nullable for more information.
+
+#if !NULLABLE_ATTRIBUTES_DISABLE
 #nullable enable
 
 namespace System.Diagnostics.CodeAnalysis
@@ -225,7 +252,6 @@ namespace System.Diagnostics.CodeAnalysis
             ReturnValue = returnValue;
         }
     }
-
 }
 
 #nullable restore
